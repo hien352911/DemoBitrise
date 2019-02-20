0cd8bb2b0386c34209543c9b49d026420e06b2d6
hien.bui <hien.bd@seesaa.vn>
2019-02-20 14:25:29 +0700

    init commit

---
+++ DemoBitrise/DemoBitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoBitrise
+//
+//  Created by seesaa on 2/20/19.
+//  Copyright © 2019 seesaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
